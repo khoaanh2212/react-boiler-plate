df91ecb8f0f10bd9bef76b6b95d994238705a524
Ka Nguyen <khoaanh2212@gmail.com>
2017-08-04 10:35:10 +0700

    init

---
+++ app/components/Header/messages.js
@@ -0,0 +1,17 @@
+/*
+ * HomePage Messages
+ *
+ * This contains all the text for the HomePage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  home: {
+    id: 'link_bird.components.Header.home',
+    defaultMessage: 'Home',
+  },
+  features: {
+    id: 'link_bird.components.Header.features',
+    defaultMessage: 'Features',
+  },
+});
---
+++ app/containers/LocaleToggle/messages.js
@@ -0,0 +1,17 @@
+/*
+ * LocaleToggle Messages
+ *
+ * This contains all the text for the LanguageToggle component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  en: {
+    id: 'link_bird.containers.LocaleToggle.en',
+    defaultMessage: 'en',
+  },
+  de: {
+    id: 'link_bird.containers.LocaleToggle.de',
+    defaultMessage: 'de',
+  },
+});
